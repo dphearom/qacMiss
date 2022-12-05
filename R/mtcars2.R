6d408abb493efdf750fd01cf7d8188455c99090b
Robert Kabacoff <rkabacoff@wesleyan.edu>
2022-12-05 18:18:34 -0500

    fix row_miss and rename functions

---
+++ R/mtcars2.R
@@ -0,0 +1,26 @@
+#' Motor Trend Car Road Tests
+#'
+#' The data was extracted from the 1974 Motor Trend US magazine, 
+#' and comprises fuel consumption and 10 aspects of automobile 
+#' design and performance for 32 automobiles (1973–74 models). 
+#' Missing data have been randomly added to the variables 
+#' \code{mpg}, \code{hp}, and \code{am}.
+#'
+#' @format A data frame with 32 rows and 11 variables:
+#' \describe{
+#'   \item{\code{mpg}}{double. Miles/(US) gallon.}
+#'   \item{\code{cyl}}{double. Number of cylinders.}
+#'   \item{\code{disp}}{double. Displacement (cu.in.).}
+#'   \item{\code{hp}}{double. Gross horsepower.}
+#'   \item{\code{drat}}{double. Rear axle ratio.}
+#'   \item{\code{wt}}{double. Weight (1000 lbs).}
+#'   \item{\code{qsec}}{double. 1/4 mile time.}
+#'   \item{\code{vs}}{double. Engine (0 = V-shaped, 1 = straight).}
+#'   \item{\code{am}}{double. Transmission (0 = automatic, 1 = manual).}
+#'   \item{\code{gear}}{double. Number of forward gears.}
+#'   \item{\code{carb}}{double. Number of carburetors.}
+#' }
+#' @source Henderson and Velleman (1981), 
+#' Building multiple regression models interactively. 
+#' Biometrics, 37, 391–411.
+"mtcars2"
